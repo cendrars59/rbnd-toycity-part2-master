29fb2a40840efef1acb24d9f14b5cc14fad50137
Cyril Leroux <cyr.leroux@outlook.com>
2016-01-12 00:00:41 +0100

    Encoding average calculation for the product

--- lib/app.rb
+++ lib/app.rb
@@ -39,29 +39,54 @@ def separator
 	puts "                                                                       "
 end
 
-
-
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Func_Requ_01
-# Criteria : Print today's date on the report
-#
-# ###############################################################################
-# Required Specifications :
-# The generated report should have the current date at the top of the report
-#
-#
-#
-#
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
 def report_date
 	"Date of report : #{Time.now}"
 end
 
+def print_sales_label_ascii(file)
+	file.puts("                                                                                ")
+	file.puts("                                                                                ")
+	file.puts(" _________      .__                  __________                             __  ")
+	file.puts("/   _____/____  |  |   ____   ______ \______   \ ____ ______   ____________/  |_ ")
+	file.puts("\_____  \\__  \ |  | _/ __ \ /  ___/  |       _// __ \\____ \ /  _ \_  __ \   __\ ")
+	file.puts("/        \/ __ \|  |_\  ___/ \___ \   |    |   \  ___/|  |_> >  <_> )  | \/|  | ")
+	file.puts("_______  (____  /____/\___  >____  >  |____|_  /\___  >   __/ \____/|__|   |__|  ")
+	file.puts("        \/     \/          \/     \/          \/     \/|__|   ")
+	file.puts("                                                                                ")
+	file.puts("        ")
+end
+
+def print_products_label_ascii(file)
+	file.puts("                                                                                ")
+	file.puts("                                                                                ")
+	file.puts("                     _            _       ")
+	file.puts("                    | |          | |      ")
+	file.puts(" _ __  _ __ ___   __| |_   _  ___| |_ ___ ")
+	file.puts("| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|")
+	file.puts("| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\")
+	file.puts("| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/")
+	file.puts("| |                                       ")
+	file.puts("|_|     ")
+	file.puts("                                                                                ")
+	file.puts("                                                                                ")
+end
+
+def print_brands_label_ascii(file)
+	file.puts("                                                                                ")
+	file.puts("                                                                                ")
+	file.puts( " _                         _     ")
+	file.puts("| |                       | |    ")
+	file.puts("| |__  _ __ __ _ _ __   __| |___ ")
+	file.puts("| '_ \\| '__/ _` | '_ \\ / _` / __|")
+	file.puts("| |_) | | | (_| | | | | (_| \\__ \\")
+	file.puts("|_.__/|_|  \\__,_|_| |_|\\__,_|___/")
+	file.puts("                                                                                ")
+	file.puts("               ")
+end
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Func_Requ_02
-# Criteria : Print today's date on the report
+# Func_Requ_01
+# Criteria : Print today's date on the report + Sales report label in ASCII
 #
 # ###############################################################################
 # Required Specifications :
@@ -76,21 +101,12 @@ end
 
 def print_heading(file)
 	file.puts(report_date)
-	file.puts("                                                                                ")
-	file.puts("                                                                                ")
-	file.puts(" _________      .__                  __________                             __  ")
-	file.puts("/   _____/____  |  |   ____   ______ \______   \ ____ ______   ____________/  |_ ")
-	file.puts("\_____  \\__  \ |  | _/ __ \ /  ___/  |       _// __ \\____ \ /  _ \_  __ \   __\ ")
-	file.puts("/        \/ __ \|  |_\  ___/ \___ \   |    |   \  ___/|  |_> >  <_> )  | \/|  | ")
-	file.puts("_______  (____  /____/\___  >____  >  |____|_  /\___  >   __/ \____/|__|   |__|  ")
-	file.puts("        \/     \/          \/     \/          \/     \/|__|   ")
-	file.puts("                                                                                ")
-	file.puts("        ")
+  print_sales_label_ascii(file)
 end
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Func_Requ_03
+# Func_Requ_02
 # Criteria : Manage sales information related to the product onto the report
 #
 # ###############################################################################
@@ -106,19 +122,11 @@ end
 
 def make_product_section(file,products)
 
-	file.puts("                                                                                ")
-	file.puts("                                                                                ")
-	file.puts("                     _            _       ")
-	file.puts("                    | |          | |      ")
-	file.puts(" _ __  _ __ ___   __| |_   _  ___| |_ ___ ")
-	file.puts("| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|")
-	file.puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
-	file.puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
-	file.puts "| |                                       "
-	file.puts "|_|     "
-	file.puts("                                                                                ")
-	file.puts("                                                                                ")
+	# ------------------------------------------------------------------------------
+	# Product label in ASCII ART
+	# ------------------------------------------------------------------------------
 
+	print_products_label_ascii(file)
 
 	# ------------------------------------------------------------------------------
 	# For each product in the data set:
@@ -159,34 +167,17 @@ def make_product_section(file,products)
 	  # (6) Calculate and print the average discount (% or $) based off the average sales price
 		#
 		# ---------------------------------------------------------------------------
-		#file.puts "Percentage of discount: #{((toy["full-price"].to_f-(total_amount_sales/toy["purchases"].length))/toy["full-price"].to_f).round(2)*100}%"
 
-
-
-
-
-
+		file.puts("Average of discount: #{(toy["full-price"].to_f-(total_amount_sales/toy["purchases"].length)).round(2)}$")
+		file.puts "Percentage of discount: #{((toy["full-price"].to_f-(total_amount_sales/toy["purchases"].length))/toy["full-price"].to_f).round(2)*100}%"
 
 	end
-end
+end # End of the method
 
-
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Func_Requ_04
-# Criteria : Manage sales information related to the brand onto the report
-#
-# ###############################################################################
-# For each brand in the data set:
-# => (1) Print the name of the brand
-# => (2) Count and print the number of the brand's toys we stock
-# => (3) Calculate and print the average price of the brand's toys
-# => (4) Calculate and print the total sales volume of all the brand's toys combined
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Simple method to get the list of brands according the JSON file provided
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
 def get_the_list_of_brands(file)
 	brands = Array.new
 	file["items"].each do |toy|
@@ -204,34 +195,37 @@ def get_the_list_of_brands(file)
 end
 
 
-
-
-
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Func_Requ_03
+# Criteria : Manage sales information related to the brand onto the report
+#
+# ###############################################################################
+# For each brand in the data set:
+# => (1) Print the name of the brand
+# => (2) Count and print the number of the brand's toys we stock
+# => (3) Calculate and print the average price of the brand's toys
+# => (4) Calculate and print the total sales volume of all the brand's toys combined
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 def make_brand_section(file,products)
-	file.puts("                                                                                ")
-	file.puts("                                                                                ")
-	file.puts( " _                         _     ")
-	file.puts("| |                       | |    ")
-	file.puts("| |__  _ __ __ _ _ __   __| |___ ")
-	file.puts("| '_ \\| '__/ _` | '_ \\ / _` / __|")
-	file.puts("| |_) | | | (_| | | | | (_| \\__ \\")
-	file.puts("|_.__/|_|  \\__,_|_| |_|\\__,_|___/")
-	file.puts("                                                                                ")
-	file.puts("               ")
 
-	brandslist = get_the_list_of_brands(products)
+	# ------------------------------------------------------------------------------
+	# Brands label in ASCII ART
+	# ------------------------------------------------------------------------------
+	print_brands_label_ascii(file)
+
 
 	# -----------------------------------------------------------------------------
 	# Genarating the report by brand according the list of brands gathered into the
 	# file.
 	# -----------------------------------------------------------------------------
 
-	brandslist.each do |brand|
+	get_the_list_of_brands(products).each do |brand|
 
 		# Var initialization by brand
-		nb_stocked_unit = 0
+
 		total_amount_sales_brand = 0
 		total_amount_purchase_brand = 0
+
 		# Products selection by brand
 		toys_list = products["items"].select do |toy|
 			toy["brand"] == brand
@@ -246,8 +240,8 @@ def make_brand_section(file,products)
 		# ---------------------------------------------------------------------------
 		# (2) Count and print the number of the brand's toys we stock
 		# ---------------------------------------------------------------------------
-
-	  toys_list.each do |toy|
+		nb_stocked_unit = 0
+		toys_list.each do |toy|
 			nb_stocked_unit = nb_stocked_unit + toy["stock"]
 		end
 		file.puts("Number of the stock units available: #{nb_stocked_unit}")
@@ -274,7 +268,6 @@ def make_brand_section(file,products)
 		# ---------------------------------------------------------------------------
 		# (4) Calculate and print the total sales volume of all the brand's toys combined
 		# ---------------------------------------------------------------------------
-
 		toys_list.each do |toy|
 			total_amount_sales_toy = 0
 			total_purchases_toys = 0
@@ -290,36 +283,9 @@ def make_brand_section(file,products)
 			total_amount_sales_brand = total_amount_sales_brand + total_amount_sales_toy
 			total_amount_purchase_brand = total_amount_purchase_brand + total_purchases_toys
 		end
-
 		file.puts( "Total amount of sales for the brand without shipping cost: #{total_amount_sales_brand.round(2)}$")
-
 	end
-
-
-
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Print data method is used to gather and fill the report information related to
@@ -327,12 +293,9 @@ end
 #
 #
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
 def print_data(file,products)
-
 		make_product_section(file,products)
 		make_brand_section(file,products)
-
 end
 
 
@@ -341,7 +304,6 @@ end
 #
 #
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
 def create_report
 	setup_files
 	print_heading($report_file)
@@ -354,7 +316,6 @@ end
 #
 #
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
 def start
 	separator
 	puts "The sales report is generating please wait for the end of execution at #{Time.now}"
@@ -382,45 +343,3 @@ start
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Tec-Requ-01
-# Criteria : The student has successfully output report information to a text file.
-#
-# ###############################################################################
-# Get path to products.json, read the file into a string,
-# and transform the string into a usable hash
-#
-#
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# For each brand in the data set:
