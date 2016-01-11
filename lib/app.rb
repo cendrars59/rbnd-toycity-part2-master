85dc8f30d7b7a63605a237a2f6c35607c25f414e
Cyril Leroux <cyr.leroux@outlook.com>
2016-01-11 21:45:43 +0100

    Encoding the methods for the sales information according the brand

--- lib/app.rb
+++ lib/app.rb
@@ -157,7 +157,7 @@ def make_product_section(file,products)
 
 		# ---------------------------------------------------------------------------
 	  # (6) Calculate and print the average discount (% or $) based off the average sales price
-		# 						
+		#
 		# ---------------------------------------------------------------------------
 		#file.puts "Percentage of discount: #{((toy["full-price"].to_f-(total_amount_sales/toy["purchases"].length))/toy["full-price"].to_f).round(2)*100}%"
 
@@ -183,17 +183,120 @@ end
 # => (4) Calculate and print the total sales volume of all the brand's toys combined
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Simple method to get the list of brands according the JSON file provided
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+def get_the_list_of_brands(file)
+	brands = Array.new
+	file["items"].each do |toy|
+		already_exist = false
+		brands.each do |brand|
+			if brand == toy["brand"]
+				already_exist = true
+			end
+		end
+		if already_exist == false
+			brands.push(toy["brand"])
+		end
+	end
+	return brands
+end
+
+
+
+
+
 def make_brand_section(file,products)
 	file.puts("                                                                                ")
 	file.puts("                                                                                ")
-	file.puts " _                         _     "
-	file.puts "| |                       | |    "
-	file.puts "| |__  _ __ __ _ _ __   __| |___ "
-	file.puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
-	file.puts "| |_) | | | (_| | | | | (_| \\__ \\"
-	file.puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+	file.puts( " _                         _     ")
+	file.puts("| |                       | |    ")
+	file.puts("| |__  _ __ __ _ _ __   __| |___ ")
+	file.puts("| '_ \\| '__/ _` | '_ \\ / _` / __|")
+	file.puts("| |_) | | | (_| | | | | (_| \\__ \\")
+	file.puts("|_.__/|_|  \\__,_|_| |_|\\__,_|___/")
 	file.puts("                                                                                ")
 	file.puts("               ")
+
+	brandslist = get_the_list_of_brands(products)
+
+	# -----------------------------------------------------------------------------
+	# Genarating the report by brand according the list of brands gathered into the
+	# file.
+	# -----------------------------------------------------------------------------
+
+	brandslist.each do |brand|
+
+		# Var initialization by brand
+		nb_stocked_unit = 0
+		total_amount_sales_brand = 0
+		total_amount_purchase_brand = 0
+		# Products selection by brand
+		toys_list = products["items"].select do |toy|
+			toy["brand"] == brand
+		end
+
+		# ---------------------------------------------------------------------------
+		# (1) Print the name of the brand
+		# ---------------------------------------------------------------------------
+		file.puts("***********************************************************************")
+		file.puts("Sales information for the brand #{brand}")
+
+		# ---------------------------------------------------------------------------
+		# (2) Count and print the number of the brand's toys we stock
+		# ---------------------------------------------------------------------------
+
+	  toys_list.each do |toy|
+			nb_stocked_unit = nb_stocked_unit + toy["stock"]
+		end
+		file.puts("Number of the stock units available: #{nb_stocked_unit}")
+
+
+		# ---------------------------------------------------------------------------
+		# (3) Calculate and print the average price of the brand's toys
+		# Average price = sum of the toy's prices / number of toys
+		# ---------------------------------------------------------------------------
+
+		total_sum_prices = 0
+		total_toys_ref = 0
+		toys_list.each do |toy|
+
+			# Calculate the total amount of sales for one toy reference for one brand
+			# For one refrence , make the sum of the amount of the purchases
+
+			total_sum_prices = total_sum_prices + toy["full-price"].to_f
+			total_toys_ref = total_toys_ref + 1
+		end
+		file.puts "Number of toys references #{total_toys_ref}"
+		file.puts "Average price of the brand toys: #{(total_sum_prices/total_toys_ref).round(2)}$"
+
+		# ---------------------------------------------------------------------------
+		# (4) Calculate and print the total sales volume of all the brand's toys combined
+		# ---------------------------------------------------------------------------
+
+		toys_list.each do |toy|
+			total_amount_sales_toy = 0
+			total_purchases_toys = 0
+			# Calculate the total amount of sales for one toy reference for one brand
+			# For one refrence , make the sum of the amount of the purchases
+			toy["purchases"].each do |purchase|
+				total_amount_sales_toy = total_amount_sales_toy + purchase["price"]
+				total_purchases_toys = total_purchases_toys + 1
+			end
+
+			# Calculate and print the total amount of sales for all toys references for one brand
+			# Make the sum of the totals of the purchases by produduct
+			total_amount_sales_brand = total_amount_sales_brand + total_amount_sales_toy
+			total_amount_purchase_brand = total_amount_purchase_brand + total_purchases_toys
+		end
+
+		file.puts( "Total amount of sales for the brand without shipping cost: #{total_amount_sales_brand.round(2)}$")
+
+	end
+
+
+
 
 end
 
